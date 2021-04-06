0f2dec15d5a324dd1253087123f53dce3d933168
carlirows <carlirows@gmail.com>
2021-04-05 23:19:34 -0500

    sign up with email and password, site writes users into database

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -11,8 +11,34 @@ const config = {
     appId: "1:169664152028:web:574b3836eb6d7d72be8f19",
     measurementId: "G-HJZL45GPFC"
   }
+  !firebase.apps.length ?
+  firebase.initializeApp(config) :
+  firebase.app()
+  
+  export const createUserProfileDocument = async (userAuth, additionalData) => {
 
-  firebase.initializeApp(config)
+    if(!userAuth) return
+    const userRef = firestore.doc(`users/$${userAuth.uid}`)
+    const snapShot = await userRef.get()
+    
+    if(!snapShot.exists) {
+      const { displayName, email } = userAuth
+      const createdAt = new Date()
+
+      try {
+        await userRef.set({
+          displayName,
+          email,
+          createdAt,
+          ...additionalData
+        })
+      } catch (e) {
+        console.log('error creating user', e.message)
+      }
+    }
+    return userRef
+  }
+  
 
   export const auth = firebase.auth()
   export const firestore = firebase.firestore()
